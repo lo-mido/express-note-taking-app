659685f94ad94ea57306c61cf8aeab3ca5c16b6f
lourdes-dorvilus <lourdesdorvilus@yahoo.com>
2023-08-28 18:07:07 -0400

    set up helper file with some fs functions to help out with routesd

---
+++ helpers.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const util = require("util");
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
+
+const readAndDelete = (id, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedData = JSON.parse(data);
+        const filteredData = parsedData.filter((note) => {
+          return note.id !== id;
+        });
+  
+        writeToFile(file, filteredData);
+      }
+    });
+  };
+  
+  module.exports = { readFromFile, writeToFile, readAndAppend, readAndDelete };
